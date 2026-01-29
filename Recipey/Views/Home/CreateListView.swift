1ae83d5b27e04d72fe88690ef00da2420ac59e89
Julia Guzzo <jkguzzo@gmail.com>
2026-01-29 14:27:25 -0500

    Replaced CreateListView dismiss button with glass effect toolbar button to maintain consistency with AddRecipeView

--- Recipey/Views/Home/CreateListView.swift
+++ Recipey/Views/Home/CreateListView.swift
@@ -18,91 +18,83 @@ struct CreateListView: View {
     @Binding var showingAddOverlay: Bool
 
     var body: some View {
-        VStack(spacing: 35) {
-            Spacer()
-            VStack {
-                PhotosPicker(selection: $selectedPhoto,
-                             matching: .images,
-                             photoLibrary: .shared())
-                {
-                    if let image = selectedPhotoData, let uiImage = UIImage(data: image) {
-                        Image(uiImage: uiImage)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 175)
-                            .cornerRadius(10)
-                    } else {
-                        VStack {
-                            Image(systemName: "photo.fill")
-                                .font(.system(size: 100))
-                                .foregroundStyle(.secondary)
-                                .font(.largeTitle)
-                            Text("Add Photo")
+        NavigationStack {
+            VStack(spacing: 35) {
+                Spacer()
+                VStack {
+                    PhotosPicker(selection: $selectedPhoto,
+                                 matching: .images,
+                                 photoLibrary: .shared())
+                    {
+                        if let image = selectedPhotoData, let uiImage = UIImage(data: image) {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 175)
+                                .cornerRadius(10)
+                        } else {
+                            VStack {
+                                Image(systemName: "photo.fill")
+                                    .font(.system(size: 100))
+                                    .foregroundStyle(.secondary)
+                                    .font(.largeTitle)
+                                Text("Add Photo")
+                            }
+                            .padding(20)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.primary, lineWidth: 1)
+                            )
                         }
-                        .padding(20)
+                    }
+                }
+                
+                TextField("Enter your title...", text: $title)
+                    .padding(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.primary, lineWidth: 1)
+                    )
+                
+                Button {
+                    if !title.isEmpty {
+                        let list = RecipeList(title: title, image: selectedPhotoData)
+                        modelContext.insert(list)
+                        // bad practice probably
+                        try? modelContext.save()
+                        showingAddOverlay = false
+                        dismiss()
+                    } else {
+                        // error
+                    }
+                } label: {
+                    Text("Create List")
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 30)
                         .overlay(
                             RoundedRectangle(cornerRadius: 10)
                                 .stroke(.primary, lineWidth: 1)
                         )
-                    }
                 }
+                .disabled(title.isEmpty || selectedPhotoData == nil)
+                
+                Spacer()
             }
-
-            TextField("Enter your title...", text: $title)
-                .padding(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(.primary, lineWidth: 1)
-                )
-            
-            Button {
-                if !title.isEmpty {
-                    let list = RecipeList(title: title, image: selectedPhotoData)
-                    modelContext.insert(list)
-                    // bad practice probably
-                    try? modelContext.save()
-                    showingAddOverlay = false
-                    dismiss()
-                } else {
-                    // error
-                }
-            } label: {
-                Text("Create List")
-                    .padding(.vertical, 10)
-                    .padding(.horizontal, 30)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                        .stroke(.primary, lineWidth: 1)
-                    )
-            }
-            .disabled(title.isEmpty || selectedPhotoData == nil)
-            
-            Spacer()
-        }
-        .overlay {
-            VStack {
-                HStack {
-                    Spacer()
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
                     Button {
                         dismiss()
                     } label: {
                         Image(systemName: "xmark")
-                        .font(.title3)
-                        .padding(10)
-                        .background {
-                            Circle()
-                                .stroke(.primary)
-                        }
                     }
                 }
-                Spacer()
             }
-        }
-        .buttonStyle(.plain)
-        .padding(.horizontal)
-        .task(id: selectedPhoto) {
-            if let data = try? await selectedPhoto?.loadTransferable(type: Data.self) {
-                selectedPhotoData = data
+            .buttonStyle(.plain)
+            .padding(.horizontal)
+            .task(id: selectedPhoto) {
+                if let data = try? await selectedPhoto?.loadTransferable(type: Data.self) {
+                    selectedPhotoData = data
+                }
             }
         }
     }
