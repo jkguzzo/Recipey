7225a492931e77add7d8ae7c3607268e73b073ba
Julia Guzzo <jkguzzo@gmail.com>
2026-02-01 19:33:32 -0500

    Added toolbar option in ListDetailView to allow users to change names of Lists

--- Recipey/Views/Home/ListDetailView.swift
+++ Recipey/Views/Home/ListDetailView.swift
@@ -5,10 +5,14 @@
 //  Created by Julia Guzzo on 12/17/25.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ListDetailView: View {
+    @Environment(\.modelContext) var modelContext
     var list: RecipeList
+    @State private var showRenamePopover: Bool = false
+    @State private var newTitle: String = ""
     var body: some View {
             VStack {
                 if list.recipes.count == 0 {
@@ -18,10 +22,48 @@ struct ListDetailView: View {
                 }
             }
             .navigationTitle(list.title)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu("", systemImage: "pencil") {
+                        Button {
+                            showRenamePopover = true
+                        } label: {
+                            Text("Rename List")
+                        }
+                    }
+                    .popover(isPresented: $showRenamePopover, attachmentAnchor: .point(.trailing), arrowEdge: .top) {
+                        HStack {
+                            TextField(list.title, text: $newTitle)
+                            Button {
+                                showRenamePopover = false
+                                renameList()
+                            } label: {
+                                Image(systemName: "checkmark")
+                            }
+                            .buttonStyle(.plain)
+                        }
+                        .padding()
+                        .frame(width: 250, height: 75)
+                        .presentationCompactAdaptation(.popover)
+                        
+                    }
+                }
+            }
+    }
+    
+    func renameList() {
+        // TODO: add validation to make sure title is unique
+        if !newTitle.isEmpty {
+            list.title = newTitle
+            try? modelContext.save()
+        }
     }
 }
 
 #Preview {
-    let recipeList = RecipeList(title: "Breakfast", image: nil)
-    ListDetailView(list: recipeList)
+    NavigationStack {
+        let recipeList = RecipeList(title: "Breakfast", image: nil)
+        ListDetailView(list: recipeList)
+    }
 }
+
